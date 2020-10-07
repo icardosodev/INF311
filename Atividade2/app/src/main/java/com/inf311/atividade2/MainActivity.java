94425accf52efcaa2f0c15cc3ea2f65e7ce3809e
Igor Cardoso <igorgcardoso7@gmail.com>
2020-10-07 12:04:34 -0300

    Atividade 2.B

--- Atividade2/app/src/main/java/com/inf311/atividade2/MainActivity.java
+++ Atividade2/app/src/main/java/com/inf311/atividade2/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -13,6 +14,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("B", "onCreate: Main");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("B", "onStart: Main");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i("B", "onResume: Main");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.i("B", "onRestart: Main");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i("B", "onPause: Main");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i("B", "onStop: Main");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i("B", "onDestroy: Main");
     }
 
     private String getValue(int id) {
--- Atividade2/app/src/main/java/com/inf311/atividade2/ReportActivity.java
+++ Atividade2/app/src/main/java/com/inf311/atividade2/ReportActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class ReportActivity extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class ReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+        Log.i("B", "onStart: Report");
 
         Intent it = getIntent();
         Bundle params = it.getExtras();
@@ -26,6 +28,42 @@ public class ReportActivity extends AppCompatActivity {
         setText(R.id.heightReport, String.valueOf(height));
         setText(R.id.imc, String.valueOf(imc));
         setText(R.id.classify, getClassify(imc));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("B", "onStart: Report");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i("B", "onResume: Report");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.i("B", "onRestart: Report");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i("B", "onPause: Report");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i("B", "onStop: Report");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i("B", "onDestroy: Report");
     }
 
     private String getClassify(double imc) {
